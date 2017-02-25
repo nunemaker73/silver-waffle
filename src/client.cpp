58890199064dde35b0fdf08d20c0312c80a28867
Craig Nunemaker <nunemaker73@users.noreply.github.com>
2017-02-25 15:15:26 -0600

    fixed connect

--- src/client.cpp
+++ src/client.cpp
@@ -13,27 +13,30 @@ https_client::https_client(const std::string& server, const std::string& path)
 {
 	using namespace boost::asio;
 	std::string data;
-	//setup context
+	
+// Create a context that uses the default paths for
+// finding CA certificates.
 	ssl::context ctx(ssl::context::sslv23);
+	ctx.set_default_verify_paths();
 	
 	//set up ioservice
 	std::shared_ptr<io_service> ioserv(new io_service);
 	io_service_p = ioserv;
-	
-	ip::tcp::resolver resolver(io_service);
-    ip::tcp::resolver::query query(server, "https");
-    ip::tcp::resolver::iterator endpoint_it;
-    endpoint_it = resolver.resolve(query);
 
-	
 	//setup ssl stream
 	std::shared_ptr<ssl_stream_t> streamp(new ssl_stream_t(*io_service_p,ctx));
 	socket_p =  streamp;
 	
-	socket_p->lowest_layer().connect(endpoint_it);
-	
-	socket_p->handshake (ssl::stream<ip::tcp::socket>::client);
-
+	tcp::resolver resolver(*io_service);
+	tcp::resolver::query query(server, "https");
+	boost::asio::connect(socket_p->lowest_layer(), resolver.resolve(query));
+	socket_p->lowest_layer().set_option(tcp::no_delay(true));
+	// Perform SSL handshake and verify the remote host's
+	// certificate.
+	socket_p->set_verify_mode(ssl::verify_peer);
+	socket_p->set_verify_callback(ssl::rfc2818_verification(server));
+	socket_p->handshake(ssl_socket::client);
+// read and write as normal
 	data =  "GET "+ path +" HTTP/1.0\r\nHost: " + path + "r\nConnection: close\r\n\r\n";
 	write(data);
 	http_version_ = readWord();
