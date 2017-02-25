196bf20d7aa35f7a3d011b5b546dfe8d40e57d36
Craig Nunemaker <nunemaker73@users.noreply.github.com>
2017-02-24 18:55:17 -0600

    writeData needed a type

--- src/client.hpp
+++ src/client.hpp
@@ -48,7 +48,7 @@ private:
     unsigned int readInt();
     std::string readHeaders();
     std::string readAll();
-    writeData(std::string data){boost::asio::write(s,boost::asio::buffer(data));};
+    void writeData(std::string data){boost::asio::write(s,boost::asio::buffer(data));};
 };
 
 #endif
