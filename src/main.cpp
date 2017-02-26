884226da24e0820d9bd53d76967d5b72a87b3393
Craig Nunemaker <nunemaker73@users.noreply.github.com>
2017-02-26 09:49:30 -0600

    error handling could be too broad

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,14 @@
 #include <iostream>
-#include "error.hpp"
+
 #include "sec.h"
 
 int main (int argc, char *argv[])
 {
 using std::cout;
 using std::exception;
-try {
-	
-	if (argc != 2) throw basic_error("argument usage error");
+
 
 	sec::sec s(argv[1]);
-}
-catch (exception& e)
-{
-	cout << e.what()<<"\n";	
-}
+
 return 0;
 }
