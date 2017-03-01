28cbbaa450094ff6d4969c72f5d2f073da3d1c10
Craig Nunemaker <nunemaker73@users.noreply.github.com>
2017-02-28 21:51:57 -0600

    xerces error

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,9 @@
 #include <iostream>
-#include <xerces/util/PlatformUtils.hpp>
-
 #include "sec.hpp"
 
 int main (int argc, char *argv[])
 {
-using std::cout;
-using std::exception;
-
-try { xerces::XMLPlatformUtils::Initialize()}
-catch (const xerces::XMLException& toCatch)
-	{std::cout << toCatch.what();}
 
 	sec::sec s(argv[1]);
-xerces::XMLPlatformUtils::Terminate();
 return 0;
 }
